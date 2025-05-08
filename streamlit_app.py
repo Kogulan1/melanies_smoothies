fcbae80913bf352efe0071d127feefc54d8f552f
Kogulan1 <65659530+Kogulan1@users.noreply.github.com>
2025-05-08 18:00:21 +0200

    Update streamlit_app.py
    Error fixed

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,8 +22,8 @@ st.write("The current movie title is", order_name)
 
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col("SEARCH_ON"))
 pd_df = my_dataframe.to_pandas()
-st.dataframe(pd_df)
-st.stop()
+#st.dataframe(pd_df)
+#st.stop()
 
 ingredients_list = st.multiselect(
     "Choose upto 5 ingredients?",
