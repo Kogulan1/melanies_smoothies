f132c8a9a3e0fea4f04abba00f27bbc5d7958369
Kogulan1 <65659530+Kogulan1@users.noreply.github.com>
2025-05-08 17:56:50 +0200

    Update streamlit_app.py
    pandas added

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 # Import python packages
 import streamlit as st
+import pandas as pd
 from snowflake.snowpark.functions import col,when_matched
 import requests
 
@@ -20,7 +21,8 @@ order_name = st.text_input("Order name", "Life of Brian")
 st.write("The current movie title is", order_name)
 
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col("SEARCH_ON"))
-st.dataframe(data=my_dataframe, use_container_width=True)
+pd_df = my_dataframe.to_pandas()
+st.dataframe(pd_df)
 st.stop()
 
 ingredients_list = st.multiselect(
@@ -35,6 +37,10 @@ if ingredients_list:
 
     for each_fruit in ingredients_list:
       ingredients_string += each_fruit + ' '
+
+      search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+      st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
+      
       st.subheader(each_fruit + 'Nutrition Information')
       smoothiefroot_respone = requests.get("https://my.smoothiefroot.com/api/fruit/" + each_fruit)
       sf_df = st.dataframe(smoothiefroot_respone.json(), use_container_width = True)
